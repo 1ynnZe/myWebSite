89b2fb8e12c9a0ff5fe1bd43083a32568905b682
GuoTze <58629876+1ynnZe@users.noreply.github.com>
2023-04-09 16:27:23 +0800

    gmail

--- js/custom.js
+++ js/custom.js
@@ -133,7 +133,23 @@ $(document).ready(function () {
 	})
 
 
+	$("#gform").submit((e) => {
+		e.preventDefault()
+		$.ajax({
+			url: "",
+			data: $("gform").serialize(),
+			method: "post",
+			success: function (response) {
+				alert("Form submitted successfully")
+				window.location.reload()
+				//window.location.href="https://google.com"
+			},
+			error: function (err) {
+				alert("Something Error")
 
+			}
+		})
+	})
 
 
 
